04f25716e4bb3dc30fe3cc2fd8931fd80f22ff4d
沐风 <67620912+mufeng233@users.noreply.github.com>
2021-02-11 11:13:22 +0800

    Update Doc.php

--- Include/Doc.php
+++ Include/Doc.php
@@ -10,10 +10,15 @@ if(!$id){
 	addApiAccess($id);
 	// 根据ID查询接口是否是正常
 	$result = $db->query("SELECT name,status FROM `mxgapi_api` WHERE `id`='{$id}';")->fetch_assoc();
-	$status = $result['status'];
-	if($status == '0'){
-		include __DIR__.'/error.php';
-		exit;
+	if(!$result){
+	    include __DIR__.'/404.php';
+    	    exit;
+	}else{
+    	    $status = $result['status'];
+    	    if($status == '0'){
+    		include __DIR__.'/error.php';
+    		exit;
+    	    }
 	}
 }
 
